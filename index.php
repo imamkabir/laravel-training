17579790f50d48d16359ac3ae1a5a0e1ed401f0b
Hussaini Kabiru Garba <h.garba@iconicuniversity.edu.ng>
2025-11-26 12:55:04 +0100

    PHP basics implemented: dynamic variables, echo statements, and mixing HTML with server-side output

--- index.php
+++ index.php
@@ -5,11 +5,21 @@
 </h1>
 <h1>
     <?php
-    echo "the world is good";
+    echo "the world, " . "is good";
     ?>
 </h1>
 <p>
     <?php
+
     echo "echo and print are basicaly the same thing just stick with echo";
     ?>
 </p>
+<h2>
+    <?php
+    echo "wee " . "roman";
+    ?>
+</h2>
+<?php
+$name = "Imam";
+echo "$name " . "hello"
+?>
